3322e7043eddb04f4ad18fe19e0fbb46b5b30822
Valouoff <guillaisvalentin@gmail.com>
2026-04-08 04:31:59 +0200

    Page home : hero header

--- view/home.php
+++ view/home.php
@@ -1,1 +1,9 @@
-<h1>Page d'accueil</h1>
+<section class="heroHeader">
+    <h1>L'Ingénierie du <span>Prestige</span></h1>
+    <div class="cardPreview">
+        <div class="content">
+            <h3>POCKIT NFC</h3>
+            <P>Polycarbonate Premium</P>
+        </div>
+    </div>
+</section>
